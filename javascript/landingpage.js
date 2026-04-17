70eb0e6511c5795c8670cd421f1ee8e4935f9cba
Victoriano <ev_leodriguez@hotmail.com>
2026-04-16 23:35:25 -0300

    Completo

--- javascript/landingpage.js
+++ javascript/landingpage.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
     $("body").css({
-        transform: "translateY(100vh)",
+        transform: "translateY(100%)",
         opacity: 0.7
     });
+        if(localStorage.getItem("tocarMusica")=="true"){
+        $(document).one("click", function(){
+            var audio = $("#musica")[0];
+            audio.volume = 0.5;
+            audio.play();
+            localStorage.removeItem("tocarMusica");
+        });
+}
     setTimeout(function(){
         $("body").css({
             transition: "transform 0.5s ease, opacity 0.5s",
